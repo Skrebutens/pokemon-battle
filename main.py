115f7f45e2808da1a24cbc1fbbaa58858347bd5d
Skrebutens <149562137+Skrebutens@users.noreply.github.com>
2024-03-06 10:20:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ pokemons_file.close() # Closing file
 
 print(pokemons[0])
 
+def total(pokemons):
+    return int(pokemons["total"])
+
 while True:
     print("1. Show pokemon by index")
     print("2. Top 10 strongest pokemons (by total)")
@@ -36,15 +39,36 @@ while True:
     choice = input("Enter your choice (1-5): ")
 
     if choice == '1':
-        # https://www.w3schools.com/python/python_dictionaries_access.asp
+        pokemon_index = int(input("Choose your pokemon: "))
+        print(pokemons[pokemon_index])
         pass
     elif choice == '2':
-        # https://www.w3schools.com/python/python_lists_sort.asp
+        pokemons.sort(key = total, reverse = True)
+        print(pokemons[0:10])
         pass
     elif choice == '3':
-        # https://www.w3schools.com/python/python_lists_sort.asp
+        def total(pokemons):
+            return int(pokemons["total"])
+        pokemons.sort(key = total, reverse = False)
+        print(pokemons[0:10])
         pass
     elif choice == '4':
+        print("---POKEMON BATTLE---")
+        player = int(input("Choose your pokemon for the battle: "))
+        print(pokemons[player])
+        print("...")
+        print("Computer are choosing the pokemon")
+        random_pokemons = random.sample(pokemons, 1)
+        print("Computer chose ", random_pokemons)
+
+        while player_health > 0 and enemy_health:
+            print("")
+            enemy_damage = pokemons[enemy].get("attack") - pokemons[player].get("defense") + random.randit(5, 20)
+            player_health = pokemons[player].get("HP") - enemy_damage
+            
+
+        
+        
         # Battle
         # 
         # https://www.w3schools.com/python/ref_random_choice.asp - random choice
@@ -61,5 +85,4 @@ while True:
         print("Invalid choice, choose from 1 to 5")
 
     print("==========================")
-
 
